b5ee04a051afadaa258fac655cba996e4f19403e
Maxim Rudin <rudin.maksimka@bk.ru>
2023-07-21 15:15:22 +0500

    ending main page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,36 @@
+import Link from 'next/link';
 import style from './Footer.module.scss';
 
 export default function Footer() {
 	return (
-		<footer className={style.style}>
-			<div className='container'></div>
+		<footer className={style.Footer}>
+			<div className='container'>
+				<div className={style.row}>
+					<div className={style.colum}>
+						<h5>Навигация</h5>
+						<ul>
+							<li>
+								<Link href={'/'}>Главная</Link>
+							</li>
+							<li>
+								<Link href={'/Information'}>Информация</Link>
+							</li>
+							<li>
+								<Link href={'/Sale'}>Товары</Link>
+							</li>
+						</ul>
+					</div>
+					<div className={style.colum}>
+						<h5>Помощь</h5>
+					</div>
+					<div className={style.colum}>
+						<h5>Правила</h5>
+					</div>
+				</div>
+				<p className={style.copyright}>
+					Весь контент защищен авторским правом &copy; ФАЦ
+				</p>
+			</div>
 		</footer>
 	);
 }

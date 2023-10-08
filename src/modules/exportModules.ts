45be4fc46cf3bf7b32124e5fd26c8a54e8c9dde9
MaxRudin <rudin.maksimka@bk.ru>
2023-10-08 23:23:16 +0500

    ed

--- src/modules/exportModules.ts
+++ src/modules/exportModules.ts
@@ -4,10 +4,8 @@ import StoreSection from './StoreSection/Store';
 import InformationSection from './InformationSection/InformationSection';
 import NewsSection from './NewsSection/NewsSection';
 import Connect from './ConnectForm/Connect';
-import Conteiner from '@/components/Conteiner/conteiner';
 
 export {
-	Conteiner,
 	StartingSection,
 	AboutSection,
 	StoreSection,
